5cfe9997df730610b8eb684f2087093fed4dcd25
Leonardo Capogna <leonardocapogna@gmail.com>
2020-12-04 16:07:01 +0100

    js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,27 @@
+// Con Vue,
+//stampare a schermo un messaggio all’interno di un h1,
+//  utilizzando i data.
+// Bonus: Aggiungere un’immagine presa anch’essa da un data.
+
+// var app = new Vue(
+// {
+// el: '#root',
+//   data: {
+//     message:'hi!!'
+//   }
+// });
+
 var app = new Vue(
-{
-el: '#root',
-  data: {
-    message:'hi!!'
-  }
-});
+  {
+    el: "#container",
+    data: {
+      message: "",
+      }
+    }
+
+    el: "#container2",
+    data: {
+      text : "",
+      }
+    }
+);
